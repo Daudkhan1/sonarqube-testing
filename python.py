d0264387da47c5c6d552c5e1f6108ec6f5948e34
Daudkhan1 <daudidree1024@gmail.com>
2025-09-01 17:47:07 +0500

    message

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 # Introduce a potential security issue: hardcoded password
 password = "SuperSecret123"  # semgrep should catch hardcoded credentials
 
-# Introduce a bad function usage: eval()
+#  Introduce a bad function usage: eval()
 user_input = "2 + 2"
 result = eval(user_input)  # semgrep rule p/ci should catch unsafe eval usage
 
