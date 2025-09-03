2ee2173315274da71c1449bdd147a64031d93876
Daudkhan1 <daudidree1024@gmail.com>
2025-09-03 15:44:08 +0500

    message

---
+++ gitleak.js
@@ -0,0 +1,8 @@
+// index.js
+
+// Hardcoded secret
+const JWT_SECRET = "myjwtsecretkey";
+
+function login() {
+    console.log("Using secret:", JWT_SECRET);
+}
--- gitleak.py
+++
@@ -1,9 +0,0 @@
-# app.py
-
-# Hardcoded secret (should be detected)
-API_KEY = "1234567890abcdef"
-
-DB_PASSWORD = "SuperSecretPassword"
-
-def connect_db():
-    print(f"Connecting with password: {DB_PASSWORD}")
